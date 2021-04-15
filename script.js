264706a1d13bbfba1d9166f12d68fa23f8c77b83
prabakaranpkv <82376928+prabakaranpkv@users.noreply.github.com>
2021-04-15 09:36:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+// CREATE a Reqeust INSTANCE
+var req = new XMLHttpRequest();
+//initates a connection or create a connection
+req.open('GET','https://restcountries.eu/rest/v2/all',true);
+//sending the request
+req.send();
+//load the function
+//this function will be triggered only when the data has been received successfully;
+req.onload=function(){
+var data=JSON.parse(this.response);
+for(var i=0; i<data.length;i++)
+{
+    console.log(data[i].name);
+}
+}
+
